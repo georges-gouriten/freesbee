1a855f47e8d42e2c603ba6b5d840d040407e673d
Vince Martinez <eykosioux@gmail.com>
2011-11-04 23:44:22 +0100

    New rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coldcut::Application.config.secret_token = 'd1118cfde6eff7e4196aba251ddb5b8d30cdbeccbf3729d8debab8551bd6909d11da5fdf6f1851e84d0bfbe65a9e9513e28ee9c034273b950cb8cb435a85503d'
