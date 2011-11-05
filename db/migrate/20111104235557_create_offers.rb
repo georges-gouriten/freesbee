82fb590ed1bdcd9a7eba55a42314b5d6f3ba1255
Vince Martinez <eykosioux@gmail.com>
2011-11-05 03:01:47 +0100

    Removing functional field

--- db/migrate/20111104235557_create_offers.rb
+++ db/migrate/20111104235557_create_offers.rb
@@ -1,12 +1,10 @@
 class CreateOffers < ActiveRecord::Migration
   def change
     create_table :offers do |t|
-      t.string :name
+      t.string :title
       t.string :description
       t.string :photo
       t.string :pickup_point
-      t.boolean :is_functional
-      
       t.timestamps
     end
   end
